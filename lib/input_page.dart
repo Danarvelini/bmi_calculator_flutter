3a112dbe86db2ab1969cc8a5127ff09ded90b907
danhausen <danihausen@live.com>
2021-03-14 19:52:40 -0300

    main

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,51 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableWidget(color: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableWidget(color: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableWidget(color: Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableWidget(color: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableWidget(color: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableWidget extends StatelessWidget {
+  ReusableWidget({this.color});
+  Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
